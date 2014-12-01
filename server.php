f95656652699c39b767e1bc217d36b3989946287
netosolis <netosolis@enterware.com.mx>
2014-11-30 19:25:19 -0600

    :smile:

---
+++ server.php
@@ -0,0 +1,19 @@
+<?php
+	//importamos la libreria de pusher
+	require('libs/Pusher.php');
+	//comprobamos que llego un mensaje por el metodo GET
+	if($_GET){
+		if(isset($_GET['usuario']) && isset($_GET['mensaje']) && isset($_GET['emoticone'])){
+
+			//Checamos que los mensages no contengan codigo html ni php a menos que sean emoticones...
+			//Esto es para evitar ataques
+			$usuario = strip_tags($_GET['usuario']);
+			if(!$_GET['emoticone'])$mensaje = strip_tags($_GET['mensaje']);
+			else $mensaje = $_GET['mensaje'];
+
+			/*creamos un objeto pusher que recibe como parametros APP_KEY, APP_SECRET, APP_ID */
+			$pusher = new Pusher('APP_KEY', 'APP_SECRET', 'APP_ID');
+			$pusher->trigger('chat', 'mensaje', array('usuario'=> $usuario,'mensaje' => $mensaje) );
+		}
+	}
+?>

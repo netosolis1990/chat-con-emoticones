2e58c982c30f557ebeaf1fef4dd3f538725aff3b
Ernesto Solis <ernesto.cabrera@upt.edu.mx>
2014-12-01 20:44:55 -0600

    Update server.php

--- server.php
+++ server.php
@@ -8,9 +8,9 @@
 			//Checamos que los mensages no contengan codigo html ni php a menos que sean emoticones...
 			//Esto es para evitar ataques
 			$usuario = strip_tags($_GET['usuario']);
+			if(strcmp($usuario, '') == 0)return;
 			if($_GET['emoticone']=='false')$mensaje = strip_tags($_GET['mensaje']);
 			else $mensaje = $_GET['mensaje'];
-
 			/*creamos un objeto pusher que recibe como parametros APP_KEY, APP_SECRET, APP_ID */
 			$pusher = new Pusher('APP_KEY', 'APP_SECRET', 'APP_ID');
 			$pusher->trigger('chat', 'mensaje', array('usuario'=> $usuario,'mensaje' => $mensaje) );
